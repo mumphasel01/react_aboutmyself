cbedfa97fdfe4ba05d3f204113f9cac3a38dc04c
Oluwo Mumeen <mumphasel01@gmail.com>
2023-05-09 21:17:26 +1000

    reactaboutmyself

---
+++ firstIntro/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>ABOUT MY SELF</h1>
+      <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg" alt="Profile picture" />
+      <p>My name is Oluwo Mumini Dayo</p>
+      <p>I'm 20 years old and versatile with learning new things</p>
+      <p>I reside at Dada Estate in Osogbo</p>
+      <p>I like making friends all round the globe</p>
+      <p>I start my journey of learing code in year 2023</p>
+      <p>We started the coding class as a newbie but now we thank God that we are going Pro</p>
+      <p>I beleive learning code is something the will want to interest anyone to learn but when you started you feel like run away</p>
+      <p>Just like what everyone know that being a coder you should be always prepare to face challenges about debug of code</p>
+      <p>At last I love to a developer and problem solver to the world at large</p>
+    </div>
+  
+  )
+}
+
+export default App
